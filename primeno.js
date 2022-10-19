b7e3d20ce3a05ef2a20124644f9c6f131c586a7d
notlegal96 <shenm09@gmail.com>
2022-10-19 23:28:10 +0530

    prime_and_palindrome

---
+++ primeno.js
@@ -0,0 +1,11 @@
+let a = 0;
+    for(let i=0; i>=num; i++){
+        if(num%i==0){
+            a++
+        }
+    }if (a!=2){
+        console.log("Yes");
+        
+    }else{
+        console.log("No");
+    } 
